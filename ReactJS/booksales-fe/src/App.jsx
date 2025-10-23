d5e9196bd78acc78323f60c590254cc4f17dd5d6
ferdiendahas <ferdiendahas@gmail.com>
2025-10-24 06:42:16 +0700

    React lanjutan tugas 01

---
+++ ReactJS/booksales-fe/src/App.jsx
@@ -0,0 +1,66 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/public"
+import PublickLayout from "./layouts/public"
+import Books from "./pages/public/books"
+import Login from "./pages/auth/login"
+import Register from "./pages/auth/register"
+import AdminLayout from "./layouts/admin"
+import Dashboard from "./pages/admin"
+import AdminBooks from "./pages/admin/books"
+import BookCreate from "./pages/admin/books/create"
+import AdminGenres from "./pages/admin/genres"
+import GenreCreate from "./pages/admin/genres/create"
+import AdminAuthors from "./pages/admin/authors"
+import AuthorCreate from "./pages/admin/authors/create"
+
+function App() {
+
+  return (
+    <>
+        <BrowserRouter>
+            <Routes>
+                // public
+                <Route element ={<PublickLayout />}>
+                    <Route index element ={<Home/>}/>
+
+
+                    <Route path="books">
+                        <Route index element={<Books />} />
+                    </Route>
+                 </Route>    
+
+
+                // authenticated
+                <Route path="login" element ={<Login />}/>
+                <Route path="register" element ={<Register/>}/>
+
+                // admin
+                <Route path="admin" element = {<AdminLayout/>}>
+                    <Route index element ={<Dashboard/>}/>
+
+                    <Route path="authors">
+                        <Route index element ={<AdminAuthors />}/>
+                        <Route path="create" element ={<AuthorCreate />}/>
+                    </Route> 
+
+                    <Route path="genres">
+                        <Route index element ={<AdminGenres />}/>
+                        <Route path="create" element ={<GenreCreate />}/>
+                    </Route> 
+
+                    <Route path="books">
+                        <Route index element ={<AdminBooks />}/>
+                        <Route path="create" element ={<BookCreate />}/>
+                    </Route>
+
+                </Route>
+            </Routes>
+
+
+        </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
---
+++ ReactJS/booksales-fe/src/layouts/public.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
+
+export default function PublickLayout() {
+    return (
+        <>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
